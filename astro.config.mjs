20685c01a0c04739af5995129bf9bc48d65f9f59
CryptoNahue <cryptonahue@gmail.com>
2024-06-23 12:27:04 -0300

    feat: install tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://descilatam.org',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()],
   i18n: {
     defaultLocale: 'es',
     locales: ['en', 'es', 'pt'],
