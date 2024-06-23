f10e6c4a943f2fa36165021c23c9416143210aa5
CryptoNahue <cryptonahue@gmail.com>
2024-06-23 13:29:02 -0300

    feat: index translation

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://descilatam.org',
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind({applyBaseStyles: false,})],
   i18n: {
     defaultLocale: 'es',
     locales: ['en', 'es', 'pt'],
