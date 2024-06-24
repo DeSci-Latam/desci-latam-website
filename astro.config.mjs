0e813bd7c8cdb8c211c5fd723d617bc61bc0807f
CryptoNahue <cryptonahue@gmail.com>
2024-06-24 05:19:00 -0300

    feat: change site config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dev-descilatam.vercel.app',
+ /*  site: 'https://dev-descilatam.vercel.app', */
   
   integrations: [mdx({
     syntaxHighlight: 'shiki',
