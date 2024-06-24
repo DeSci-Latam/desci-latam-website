13ea8a6e26bde8ed659da9065e5ad1b4256c1438
CryptoNahue <cryptonahue@gmail.com>
2024-06-24 03:34:09 -0300

    fix: deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://descilatam.org',
+  site: 'dev-descilatam.vercel.app',
   
   integrations: [mdx({
     syntaxHighlight: 'shiki',
