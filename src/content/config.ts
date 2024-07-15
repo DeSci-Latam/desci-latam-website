f7a1a89b93f251d6d8fb5a987b28a6983c492e61
CryptoNahue <cryptonahue@gmail.com>
2024-07-15 04:36:22 -0300

    feat: add categories and filters

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,43 @@
 import { defineCollection, z } from 'astro:content';
 import { CATEGORIES } from '@/data/categories'
 
+// Post collection schema
+const postsCollection = defineCollection({
+  schema: z.object({
+    id: z.string().optional(),
+    title: z.string(),
+    meta_title: z.string().optional(),
+    description: z.string().optional(),
+    date: z.date().optional(),
+    image: z.string().optional(),
+    authors: z.array(z.string()).default(["admin"]),
+    categories: z.array(z.string()).default(["others"]),
+    tags: z.array(z.string()).default(["others"]),
+    draft: z.boolean().optional(),
+  }),
+});
+
+
+// Author collection schema
+const authorsCollection = defineCollection({
+  schema: z.object({
+    id: z.string().optional(),
+    title: z.string(),
+    meta_title: z.string().optional(),
+    image: z.string().optional(),
+    description: z.string().optional(),
+    social: z
+      .object({
+        facebook: z.string().optional(),
+        twitter: z.string().optional(),
+        instagram: z.string().optional(),
+      })
+      .optional(),
+    draft: z.boolean().optional(),
+  }),
+});
+
+
  const blog2 = defineCollection({
   type: 'content',
   schema: ({ image }) =>
@@ -98,7 +135,7 @@ const releases = defineCollection({
     }),
 });
 
-export const collections = { blog, blog2, changelog, docs, guides, releases };
+export const collections = {  authors: authorsCollection,  posts: postsCollection, blog, blog2, changelog, docs, guides, releases };
 
 
 /* export const collections = { blog , changelog };
