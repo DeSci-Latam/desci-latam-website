5033012ce54541f7b25937c027f35951fa599446
cryptonahue <cryptonahue@gmail.com>
2024-11-24 18:13:36 -0300

    fix search

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,17 @@ import tailwind from '@astrojs/tailwind';
 import react from "@astrojs/react";
 import icon from "astro-icon";
 import simpleStackForm from "simple-stack-form";
+
 const PROD_URL = 'https://dev-descilatam-website.vercel.app';
 const DEV_URL = 'https://dev-descilatam-website.vercel.app';
-
-
-// Detecta si estamos en producción
 const isProduction = process.env.NODE_ENV === 'production';
 
-// https://astro.build/config
 export default defineConfig({
   site: isProduction ? PROD_URL : DEV_URL,
+  output: 'static',  // Asegura que sea static para Pagefind
+  build: {
+    format: 'directory'
+  },
   i18n: {
     defaultLocale: 'es',
     locales: ['en', 'es', 'pt'],
@@ -23,20 +24,27 @@ export default defineConfig({
       redirectToDefaultLocale: false
     }
   },
-  integrations: [mdx({
-    syntaxHighlight: 'shiki',
-    shikiConfig: {
-      experimentalThemes: {
-        light: 'vitesse-light',
-        dark: 'material-theme-palenight'
+  integrations: [
+    mdx({
+      syntaxHighlight: 'shiki',
+      shikiConfig: {
+        experimentalThemes: {
+          light: 'vitesse-light',
+          dark: 'material-theme-palenight'
+        },
+        wrap: true
       },
-      wrap: true
-    },
-    drafts: true,
-    gfm: true
-  }), icon(), sitemap(), react(), simpleStackForm(), tailwind({
-    applyBaseStyles: false
-  })],
+      drafts: true,
+      gfm: true
+    }), 
+    icon(), 
+    sitemap(), 
+    react(), 
+    simpleStackForm(), 
+    tailwind({
+      applyBaseStyles: false
+    })
+  ],
   markdown: {
     drafts: true,
     shikiConfig: {
